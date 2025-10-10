555156c7fe3c100c1e8ca0e660da9f5028134b48
Jônatas Meireles <jonales@gmail.com>
2025-10-10 14:56:44 -0300

    Create index.ts

---
+++ nodes/Oracle/core/index.ts
@@ -0,0 +1,10 @@
+// Exportações centralizadas
+export * from './connection';
+export * from './connectionPool';
+export * from './aqOperations';
+export * from './bulkOperations';
+export * from './plsqlExecutor';
+export * from './transactionManager';
+export * from './utils/error-handler';
+export * from './types/oracle.credentials.type';
+export * from './interfaces/database.interface';
